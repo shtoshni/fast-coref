5780f1a7ba3735bc08f7cbf142d8b47ca0440284
shtoshni92 <shtoshni@ttic.edu>
2022-04-01 09:34:08 -0400

    Sonar updates

--- src/data_processing/constants.py
+++ src/data_processing/constants.py
@@ -4,9 +4,13 @@ SPEAKER_END = '[SPEAKER_END]'
 MODEL_TO_MAX_LEN = {
     'longformer': 4096,
     'spanbert': 512,
+    'bigbird': 4096,
+    'bert-dutch': 512,
 }
 
 MODEL_TO_MODEL_STR = {
     'longformer': 'allenai/longformer-large-4096',
     'spanbert': 'bert-base-cased',
+    'bigbird': 'flax-community/pino-bigbird-roberta-base',
+    'bert-dutch': 'GroNLP/bert-base-dutch-cased'
 }
--- src/data_processing/utils.py
+++ src/data_processing/utils.py
@@ -92,7 +92,8 @@ def parse_args():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('input_dir', type=str, help="Input directory.")
 	parser.add_argument('-output_dir', type=str, default=None, help="Output directory.")
-	parser.add_argument('-model', default='longformer', choices=['longformer', 'bert', 'roberta', 'spanbert'],
+	parser.add_argument('-model', default='longformer',
+	                    choices=['longformer', 'bert', 'roberta', 'spanbert', 'bigbird', 'bert-dutch'],
 	                    type=str, help="Model type.")
 	parser.add_argument('-seg_len', default=4096, type=int, help="Max. segment length")
 	parser.add_argument('-add_speaker', default=False, action="store_true",
