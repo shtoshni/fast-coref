7591b020aaae639e0e0e6ecad0c9d76f6620b030
shtoshni92 <shtoshni@ttic.edu>
2022-05-01 00:00:02 -0400

    Minor update for ontonotes upstream model

--- src/model/entity_ranking_model.py
+++ src/model/entity_ranking_model.py
@@ -105,7 +105,7 @@ class EntityRankingModel(nn.Module):
         if meta_params.use_genre_feature:
             doc_class = document["doc_key"][:2]
             if doc_class in meta_params.genres:
-                doc_class_idx = meta_params.index(doc_class)
+                doc_class_idx = meta_params.genres.index(doc_class)
             else:
                 doc_class_idx = meta_params.index(
                     meta_params.default_genre
