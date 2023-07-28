94d1f34ed7d56a3dc678bc354d2e58bb2bc093ce
Shubham Toshniwal <shtoshni@ttic.edu>
2023-07-28 15:29:10 -0400

    Fix sorting score
    Based on issue raised here - https://github.com/shtoshni/fast-coref/issues/19

--- src/model/mention_proposal/utils.py
+++ src/model/mention_proposal/utils.py
@@ -6,7 +6,7 @@ from torch import Tensor
 def sort_mentions(
     ment_starts: Tensor, ment_ends: Tensor, return_sorted_indices=False
 ) -> Tuple:
-    sort_scores = ment_starts + 1e-5 * ment_starts
+    sort_scores = ment_starts + 1e-5 * ment_ends
     _, sorted_indices = torch.sort(sort_scores, 0)
 
     output: Tuple = (ment_starts[sorted_indices], ment_ends[sorted_indices])
